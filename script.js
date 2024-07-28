674b38345a0f234d5f09b1a9d7ba63ba6d180a64
Hermine DEDJENE <dedjenehermine@gmail.com>
2024-07-28 03:32:03 +0100

    modification

---
+++ script.js
@@ -0,0 +1,16 @@
+function scrollToSection(sectionId) {
+    document.getElementById(sectionId).scrollIntoView({
+        behavior: 'smooth'
+    });
+}
+
+
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function(e) {
+        e.preventDefault();
+
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
